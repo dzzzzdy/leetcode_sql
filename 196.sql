f26db7f0162cb2298334b9b5f7f769b9509c9ce7
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2022-01-07 18:57:31 -0500

    Update 196.sql

--- 196.sql
+++ 196.sql
@@ -5,4 +5,9 @@ SELECT rank_table.id FROM(
       id,
       ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) as ranking
   FROM person) AS rank_table
-WHERE rank_table.ranking=1)
+WHERE rank_table.ranking=1);
+
+
+DELETE FROM Person WHERE Id NOT IN 
+(SELECT * FROM(
+    SELECT MIN(Id) FROM Person GROUP BY Email) as a)
