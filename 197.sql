8f4af7edb08b751f9cc481a77c1c104fd05dd703
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2022-01-07 19:32:14 -0500

    Update 197.sql

--- 197.sql
+++ 197.sql
@@ -1,4 +1,9 @@
 SELECT w1.id
 FROM weather w1 JOIN weather w2 
 ON DATEDIFF(w1.recorddate, w2.recorddate)=1
+WHERE w1.temperature>w2.temperature;
+
+SELECT w1.id
+FROM weather w1 JOIN weather w2 
+ON W1.recorddate=DATE_ADD(w2.recorddate,INTERVAL 1 day)
 WHERE w1.temperature>w2.temperature
